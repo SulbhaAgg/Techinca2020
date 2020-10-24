f7d0b86208cb38c9bfadcd3044af3330cdfdf30f
Nithyasri Babu <nithyasri.babu@gmail.com>
2020-10-24 15:38:09 -0400

    Added the str functions for each models

--- backend/simplystart/models.py
+++ backend/simplystart/models.py
@@ -27,9 +27,16 @@ class Organization(models.Model):
 class Tag(models.Model):
     tag = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "{}".format(self.tag)
+
 class OrganizationTag(models.Model):
     org = models.ForeignKey(Organization, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.org.name, self.tag.tag)
+
 
 class OrganizationContribution(models.Model):
     org = models.ForeignKey(Organization, on_delete=models.CASCADE)
@@ -37,8 +44,14 @@ class OrganizationContribution(models.Model):
     fund_required = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     fund_raised = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
+    def __str__(self):
+        return "{} - Required: {}, Raised: {}".format(self.org.name, self.fund_required, self.fund_raised)
+
 class UserContribution(models.Model):
     org_event = models.ForeignKey(OrganizationContribution, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount_contributed = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     comment = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return "{} {} - Organization: {}, Contribution: {}".format(self.user.first_name, self.user.last_name, self.org_event.org.name, self.amount_contributed)
