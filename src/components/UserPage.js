8cd4569921ecf9b290b88f363384a6879df9a202
rhea <rheamanuel02@gmail.com>
2020-10-25 06:42:36 -0400

    centered profile

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -6,8 +6,8 @@ export default function UserPage() {
     return (
         <div>
             <Container className="mt-5 mx-auto">
-                <Row className="d-flex">
-                    <Col className="align-self-center" md="3">
+                <Row className="">
+                    <Col className="text-right" >
                         <img className="circular" src="https://media-exp1.licdn.com/dms/image/C4E03AQG-bJ2v4cJ-bA/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=eR7aIYc5_ConfXZX1TZSubEPkzVkWQ0geoWXMjpOlwA"></img>
                     </Col>
                     <Col className="align-self-center">
@@ -30,21 +30,23 @@ export default function UserPage() {
 
                 <div className="text-center mx-auto">
 
-                    <Link  to="/organizations">
+                    <Link to="/organizations">
+
                         <Button className="display-6 mx-3">View highlighted businesess</Button>
+    
                     </Link>
                 </div>
 
-                <Row className="align-center justify-content-center">
-                    <Company name="Slick" img="https://media-exp1.licdn.com/dms/image/C4D0BAQHHW4ih0rqUBg/company-logo_200_200/0?e=1611792000&v=beta&t=o0G5VyTZIj1Lc8i3yFZOXCp-cW0JSlFJ7SZX9IjLJyQ" min={10} need={1100}></Company>
-                    <Company name="neop" img="https://media-exp1.licdn.com/dms/image/C560BAQE1rQ2LXr1BKQ/company-logo_200_200/0?e=1611792000&v=beta&t=EQsM6PVVn251lMtBdXw8xQpNhJOAZ9JD5ZcHsMRTmVc" min={20} need={900}></Company>
-                    <Company name="Skeyetech" img="https://media-exp1.licdn.com/dms/image/C4D0BAQHQYIaB09mcOw/company-logo_200_200/0?e=1611792000&v=beta&t=HIPjqfv6NypFEBbdXk3WZhP4QA77cWNveCllxfNwPBQ" min={20} need={2000}></Company>
-                    <Company name="incwo" img="https://media-exp1.licdn.com/dms/image/C560BAQHNlMZamYwHlA/company-logo_200_200/0?e=1611792000&v=beta&t=xsCXMvAFHypfsmKPPku_Is5nHq-XQ_j2mD4vy7-F4mQ" min={50} need={5000}></Company>
-                    <Company name="boredaux" img="https://media-exp1.licdn.com/dms/image/C560BAQED2Z2vp0GIsQ/company-logo_200_200/0?e=1611792000&v=beta&t=KHNiDCR0mYpyxfhU25SDQ4rOcFQZNiv6WTsL2ZxA6jA" min={15} need={9000}></Company>
-                    <Company name="Addinsoft" img="https://media-exp1.licdn.com/dms/image/C560BAQG7P1F4xLtKPA/company-logo_200_200/0?e=1611792000&v=beta&t=OOklkzF8TUc-BCTiLrc35RsSqHxe38_Nf98DPxwWgnQ" min={80} need={90000}></Company>
-                </Row>
+            <Row className="align-center justify-content-center">
+                <Company name="Slick" img="https://media-exp1.licdn.com/dms/image/C4D0BAQHHW4ih0rqUBg/company-logo_200_200/0?e=1611792000&v=beta&t=o0G5VyTZIj1Lc8i3yFZOXCp-cW0JSlFJ7SZX9IjLJyQ" min={10} need={1100}></Company>
+                <Company name="neop" img="https://media-exp1.licdn.com/dms/image/C560BAQE1rQ2LXr1BKQ/company-logo_200_200/0?e=1611792000&v=beta&t=EQsM6PVVn251lMtBdXw8xQpNhJOAZ9JD5ZcHsMRTmVc" min={20} need={900}></Company>
+                <Company name="Skeyetech" img="https://media-exp1.licdn.com/dms/image/C4D0BAQHQYIaB09mcOw/company-logo_200_200/0?e=1611792000&v=beta&t=HIPjqfv6NypFEBbdXk3WZhP4QA77cWNveCllxfNwPBQ" min={20} need={2000}></Company>
+                <Company name="incwo" img="https://media-exp1.licdn.com/dms/image/C560BAQHNlMZamYwHlA/company-logo_200_200/0?e=1611792000&v=beta&t=xsCXMvAFHypfsmKPPku_Is5nHq-XQ_j2mD4vy7-F4mQ" min={50} need={5000}></Company>
+                <Company name="boredaux" img="https://media-exp1.licdn.com/dms/image/C560BAQED2Z2vp0GIsQ/company-logo_200_200/0?e=1611792000&v=beta&t=KHNiDCR0mYpyxfhU25SDQ4rOcFQZNiv6WTsL2ZxA6jA" min={15} need={9000}></Company>
+                <Company name="Addinsoft" img="https://media-exp1.licdn.com/dms/image/C560BAQG7P1F4xLtKPA/company-logo_200_200/0?e=1611792000&v=beta&t=OOklkzF8TUc-BCTiLrc35RsSqHxe38_Nf98DPxwWgnQ" min={80} need={90000}></Company>
+            </Row>
 
             </Container>
-        </div>
+        </div >
     )
 }
