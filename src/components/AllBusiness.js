2500acd46899ed09d2814b883907a946f1b83eb1
Sulbha <33704683+SulbhaAgg@users.noreply.github.com>
2020-10-25 02:07:04 -0400

    filtering for businesses

--- src/components/AllBusiness.js
+++ src/components/AllBusiness.js
@@ -4,12 +4,21 @@ import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reac
 
 export default function AllBusiness(props) {
 
+    const [isFemale, setIsFemale] = useState("false");
+    const [diversity, setDiversity] = useState("");
+
     if (!props.data) {
         return (
             <div>Loading...</div>
         )
     }
 
+    // let isFemale = false ;
+    let isBlack = false ;
+    let isKid = false ;
+    let isLGBT = false ;
+    let isLatin = false ;
+    // let diversity = "" ;
 
     return (
         <Container>
@@ -25,19 +34,40 @@ export default function AllBusiness(props) {
 
                     <h2 className="heading"> Tags </h2>
                     <br />
-                    <Button className="link">Female Owned</Button>
-                    <Button className="link">Minority Owned</Button>
-                    <Button className="link">LGBT+ Owned</Button>
+                    <Button className="link" onClick = {() => {setIsFemale(true);  setDiversity("Female Owned") }}>Female Owned</Button>
+                    {/*  {() => { setIsFemale(true), "Female Owned" } }  */}
+                    <Button className="link" onClick = { () => {isBlack = true ;  setDiversity("Black Owned") } } >Black Owned</Button>
+                    <Button className="link" onClick = { () => { isKid = true ; setDiversity("Kids Owned") } } >Kids Owned</Button>
+                    <Button className="link" onClick = { () => { isLGBT = true ;  setDiversity("LGBTQ+ Owned") } } >LGBT+ Owned</Button>
+                    <Button className="link" onClick = { () => { isLatin = true ;  setDiversity("LatinX Owned") } } >LatinX Owned</Button>
+                    <Button className="link" onClick = { () => { setDiversity("") } } >Show All</Button>
                 </Col>
                 <Col md="10" >
                     <h1 className="heading">Trending</h1>
                     <div className="group">
                         {props.data.map((item) => (
-                            <Business img={item.img} name={item.name} fundingGoal={item.fundingGoal} fundingGot={item.fundingGot}></Business>
+                            // if item.tags.contains("isFemaleOwned") then display
+                            getBusiness(diversity, item) 
                         ))}
                     </div>
                 </Col>
             </Row>
         </Container>
     )
+}
+
+function getBusiness(diversity, item) {
+
+
+   // if( diversity === "" )
+ //   return 
+   // (
+  //      <Business tags={item.tags} img={item.img} name={item.name} fundingGoal={item.fundingGoal} fundingGot={item.fundingGot}></Business> 
+  //  );
+    if (diversity === "" || (item.tags && item.tags.indexOf(diversity) > -1 )){
+        return (
+            <Business tags={item.tags} img={item.img} name={item.name} fundingGoal={item.fundingGoal} fundingGot={item.fundingGot}></Business> 
+        ) ;
+    }
+    return <div></div>
 }
